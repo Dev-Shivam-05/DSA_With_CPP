e2d081c1c02b842ec798ff795fa1e8ab15ad8956
Shivam <shivambhadoriya1605@gmail.com>
2025-06-14 21:30:54 +0530

    Update Merge Sort

--- Algorithm_Analysis/Merge_Sort/program-1.cpp
+++ Algorithm_Analysis/Merge_Sort/program-1.cpp
@@ -2,65 +2,51 @@
 #include<vector>
 using namespace std;
 
-void merge(vector<int>& array,int start,int mid,int end)
+void merge(vector<int>& array, int start, int mid, int end)
 {
     vector<int> temp;
-    int i = start , j = mid + 1;
+    int i = start, j = mid + 1;
 
-    while (i <= mid)
+    while (i <= mid && j <= end)
     {
-        if (array[i] > array[j])
-        {
+        if (array[i] <= array[j])
             temp.push_back(array[i++]);
-        }
         else
-        {
             temp.push_back(array[j++]);
-        }
     }
 
     while (i <= mid)
-    {
         temp.push_back(array[i++]);
-    }
 
     while (j <= end)
-    {
         temp.push_back(array[j++]);
-    }
 
     for (int k = 0; k < temp.size(); k++)
-    {
         array[start + k] = temp[k];
-    }
-    
 }
 
-void merge_sort(vector<int>& array,int start,int end)
+void merge_sort(vector<int>& array, int start, int end)
 {
     if (start >= end)
-    {
         return;
-    }
 
-    int mid = start - (end - start) / 2;
+    int mid = start + (end - start) / 2;
 
-    merge_sort(array,start,mid);
-    merge_sort(array,mid + 1,end);
-    merge(array,start,mid,end);
+    merge_sort(array, start, mid);
+    merge_sort(array, mid + 1, end);
+    merge(array, start, mid, end);
 }
 
-void Display(vector<int>& array,int size)
+void Display(vector<int>& array, int size)
 {
     for (int i = 0; i < size; i++)
-    {
         cout << array.at(i) << " ";
-    }
+    cout << endl;
 }
 
 int main()
 {
-    int size,num;
+    int size;
 
     cout << "Enter The Size Of Array :- ";
     cin >> size;
@@ -74,13 +60,13 @@ int main()
         cin >> array[i];
     }
 
-    cout << endl << "The Unsorted Array Is :- ";
-    Display(array,size);
-    
-    merge_sort(array , 0 , size - 1);
+    cout << "\nThe Unsorted Array Is :- ";
+    Display(array, size);
 
-    cout << endl << "The Sorted Array Is :- ";
-    Display(array,size);
+    merge_sort(array, 0, size - 1);
+
+    cout << "\nThe Sorted Array Is :- ";
+    Display(array, size);
 
     return 0;
 }
